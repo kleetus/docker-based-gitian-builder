d807a09dcba3a338dcf52266fc675bd8b9074bf5
Chris Kleeschulte <chrisk@bitpay.com>
2016-08-11 07:57:44 -0400

    wip

--- run_driver.sh
+++ run_driver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --link builder -h driver -u debian -d --name driver --volumes-from shared -v $HOME/source/gitian-builder:/home/debian/gitian-builder -v $HOME/source/bitcoin:/home/debian/bitcoin -w /home/debian/gitian-builder driver bash -c 'sudo /home/debian/copy_keys && ssh -fN -oExitOnForwardFailure=yes -oStrictHostKeyChecking=no ubuntu@builder -L 2223:localhost:22 && tail -f /dev/null' &&
+docker run --link builder -h driver -u debian -d --name driver --volumes-from shared -w /home/debian/gitian-builder driver tail -f /dev/null 
 docker exec -it driver bash
