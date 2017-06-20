2fec4da0da079aca2f95ed1d678c41d41e238ae1
Chris Kleeschulte <chrisk@bitpay.com>
2017-06-20 09:39:02 -0400

    wip

--- run_builder.sh
+++ run_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -h builder --name builder \
--v $THISDIR/cache:/shared/cache \
--v $THISDIR/result:/shared/result \
+-v $THISDIR/cache:/shared/cache:Z \
+-v $THISDIR/result:/shared/result:Z \
 builder
